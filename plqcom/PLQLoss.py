43cb68b84b2bdeefc26731368de183a2493894dd
keepwith <823189857@qq.com>
2024-01-30 15:07:16 +0800

    minimax2plq

--- plqcom/PLQLoss.py
+++ plqcom/PLQLoss.py
@@ -1,6 +1,7 @@
 import numpy as np
-
+from plqutils import PLQProperty
 from plqcom.ReHLoss import ReHLoss
+from sympy import symbols, solve, Eq
 
 
 class PLQLoss(object):
@@ -9,6 +10,16 @@ class PLQLoss(object):
     """
 
     def __init__(self, quad_coef, type="plq", **paras):
+        # check the quad_coef
+        if len(quad_coef['a']) != len(quad_coef['b']) or len(quad_coef['a']) != len(quad_coef['c']):
+            print("The size of `quad_coef` is not matched!")
+            exit()
+
+        # check the type
+        if type not in ['plq', 'minimax']:
+            print("The type of PLQ function is not supported!")
+            exit()
+
         # minimax form input
         if type == "minimax":
             self.quad_coef, self.cutpoints, self.n_pieces = self.minimax2plq(quad_coef)
@@ -43,8 +54,8 @@ class PLQLoss(object):
         y = np.zeros_like(x)
         for i in range(self.n_pieces):
             cond_tmp = (x > self.cutpoints[i]) & (x <= self.cutpoints[i + 1])
-            y[cond_tmp] = self.quad_coef['a'][i] * x[cond_tmp] ** 2 + self.quad_coef['b'][i] * x[cond_tmp] + \
-                          self.quad_coef['c'][i]
+            y[cond_tmp] = (self.quad_coef['a'][i] * x[cond_tmp] ** 2 + self.quad_coef['b'][i] * x[cond_tmp] +
+                           self.quad_coef['c'][i])
 
         # add back the minimum value
         if self.min_val != np.inf:
@@ -53,7 +64,49 @@ class PLQLoss(object):
             return y
 
     def minimax2plq(self, quad_coef):
-        return quad_coef, np.array([0]), 1
+        """
+            convert the minimax form to the PLQ form
+        """
+        solutions = []
+        n_pieces = len(quad_coef['a'])
+        x = symbols('x', real=True)
+        for i in range(n_pieces):
+            for j in range(i + 1, n_pieces):
+                solutions += solve(Eq(quad_coef['a'][i] * x ** 2 + quad_coef['b'][i] * x + quad_coef['c'][i],
+                                      quad_coef['a'][j] * x ** 2 + quad_coef['b'][j] * x + quad_coef['c'][j]), x)
+
+        solutions = list(set(solutions))  # remove the duplicate solutions
+        cutpoints = np.sort(np.array(solutions))
+
+        if len(cutpoints) == 0:
+            ind_tmp = np.argmax(quad_coef['c'])  # just compare the function value at x=0
+            new_quad_coef = {'a': np.array([quad_coef['a'][ind_tmp]]), 'b': np.array([quad_coef['b'][ind_tmp]]),
+                             'c': np.array([quad_coef['c'][ind_tmp]])}
+            new_cutpoints = np.array([])
+            new_n_pieces = 1
+        else:
+            evals = (cutpoints[:-1] + cutpoints[1:]) / 2
+            evals = np.concatenate(([-1 + cutpoints[0]], evals, 1 + cutpoints[-1]))
+            new_quad_coef = {'a': np.array([]), 'b': np.array([]), 'c': np.array([])}
+            for i in range(len(evals) - 1):
+                ind_tmp = np.argmin(quad_coef['a'] * evals[i] ** 2 + quad_coef['b'] * evals[i] + quad_coef['c'])
+                new_quad_coef['a'] = np.append(new_quad_coef['a'], quad_coef['a'][ind_tmp])
+                new_quad_coef['b'] = np.append(new_quad_coef['b'], quad_coef['b'][ind_tmp])
+                new_quad_coef['c'] = np.append(new_quad_coef['c'], quad_coef['c'][ind_tmp])
+
+            # merge the successive intervals with the same coefficients
+            i = 0
+            while i < len(new_quad_coef['a']) - 1:
+                if (new_quad_coef['a'][i] == new_quad_coef['a'][i + 1] and new_quad_coef['b'][i] == new_quad_coef['b'][
+                        i + 1] and new_quad_coef['c'][i] == new_quad_coef['c'][i + 1]):
+                    new_quad_coef['a'] = np.delete(new_quad_coef['a'], i + 1)
+                    new_quad_coef['b'] = np.delete(new_quad_coef['b'], i + 1)
+                    new_quad_coef['c'] = np.delete(new_quad_coef['c'], i + 1)
+                    cutpoints = np.delete(cutpoints, i)
+
+            new_cutpoints = cutpoints
+            new_n_pieces = len(new_quad_coef['a'])
+        return new_quad_coef, new_cutpoints, new_n_pieces
 
     def _2ReHLoss(self):
         """
@@ -63,16 +116,16 @@ class PLQLoss(object):
         """
 
         # check the continuity and convexity of the PLQ function
-        if not self.is_continuous():
+        if not PLQProperty.is_continuous(self):
             print("The PLQ function is not continuous!")
             exit()
 
-        if not self.is_convex():
+        if not PLQProperty.is_convex(self):
             print("The PLQ function is not convex!")
             exit()
 
         # find the minimum value and knot
-        self.find_min()
+        PLQProperty.find_min(self)
 
         relu_coef, relu_intercept = [], []
         rehu_coef, rehu_intercept, rehu_cut = [], [], []
--- plqcom/plqutils/PLQProperty.py
+++ plqcom/plqutils/PLQProperty.py
@@ -1,67 +1,65 @@
 import numpy as np
 
 
-class PLQProperty:
+def is_continuous(plq_loss):
     """
-        This class is used to check the properties of PLQ functions
+        check whether the input PLQ function is continuous
+    :return: True or False
     """
-
-    def is_continuous(self, plq_loss):
-        """
-            check whether the input PLQ function is continuous
-        :return: True or False
-        """
-        # check the continuity at cut points from left to right
-        for i in range(plq_loss.n_pieces - 1):
-            if (plq_loss.quad_coef['a'][i] * plq_loss.cutpoints[i + 1] ** 2 + plq_loss.quad_coef['b'][i] *
-                    plq_loss.cutpoints[i + 1] + plq_loss.quad_coef['c'][i] != plq_loss.quad_coef['a'][i + 1] *
-                    plq_loss.cutpoints[i + 1] ** 2 + plq_loss.quad_coef['b'][i + 1] * plq_loss.cutpoints[i + 1] +
-                    plq_loss.quad_coef['c'][i + 1]):
-                return False
-
-        return True
-
-    def is_convex(self, plq_loss):
-        """
-            check whether the input PLQ function is convex
-        ":return: True or False
-        """
-        # check the second order derivatives
-        if min(plq_loss.quad_coef['a']) < 0:
+    # check the continuity at cut points from left to right
+    for i in range(plq_loss.n_pieces - 1):
+        if (plq_loss.quad_coef['a'][i] * plq_loss.cutpoints[i + 1] ** 2 + plq_loss.quad_coef['b'][i] *
+                plq_loss.cutpoints[i + 1] + plq_loss.quad_coef['c'][i] != plq_loss.quad_coef['a'][i + 1] *
+                plq_loss.cutpoints[i + 1] ** 2 + plq_loss.quad_coef['b'][i + 1] * plq_loss.cutpoints[i + 1] +
+                plq_loss.quad_coef['c'][i + 1]):
             return False
 
-        # compare the first order derivatives at cut points
-        for i in range(plq_loss.n_pieces - 1):
-            if (2 * plq_loss.quad_coef['a'][i] * plq_loss.cutpoints[i + 1] + plq_loss.quad_coef['b'][i] >
-                    2 * plq_loss.quad_coef['a'][i + 1] * plq_loss.cutpoints[i + 1] + plq_loss.quad_coef['b'][i + 1]):
-                return False
+    return True
 
-        return True
 
-    def check_cutoff(self, plq_loss):
-        """
-            check whether there exists a cutoff between the knots
-        :return:
-        """
-        # check the cutoff of each piece
-        for i in range(plq_loss.n_pieces - 1):
-            if plq_loss.quad_coef['a'][i] != 0:  # only will happen when the quadratic term is not zero
-                cutpoint = -plq_loss.quad_coef['b'][i] / (2 * plq_loss.quad_coef['a'][i])
-                if plq_loss.cutpoints[i] < cutpoint < plq_loss.cutpoints[i + 1]:  # if the cutoff is between the knots
-                    # add the cutoff to the knot list and update the coefficients
-                    plq_loss.cutpoints.add(cutpoint, i)
-                    plq_loss.quad_coef['a'].add(plq_loss.quad_coef['a'][i], i)
-                    plq_loss.quad_coef['b'].add(plq_loss.quad_coef['b'][i], i)
-                    plq_loss.quad_coef['c'].add(plq_loss.quad_coef['c'][i], i)
+def is_convex(plq_loss):
+    """
+        check whether the input PLQ function is convex
+    ":return: True or False
+    """
+    # check the second order derivatives
+    if min(plq_loss.quad_coef['a']) < 0:
+        return False
 
-    def find_min(self, plq_loss):
-        """
-            find the minimum knots and value of the PLQ function
-        :return:
-        """
-        # find the minimum value and knot
-        out_cut = plq_loss(plq_loss.cutpoints[1:-1])
-        plq_loss.min_val = min(out_cut)
-        plq_loss.min_knot = np.argmin(out_cut) + 1
-        # remove self.min_val from the PLQ function
-        plq_loss.quad_coef['c'] = plq_loss.quad_coef['c'] + (-plq_loss.min_val)
+    # compare the first order derivatives at cut points
+    for i in range(plq_loss.n_pieces - 1):
+        if (2 * plq_loss.quad_coef['a'][i] * plq_loss.cutpoints[i + 1] + plq_loss.quad_coef['b'][i] >
+                2 * plq_loss.quad_coef['a'][i + 1] * plq_loss.cutpoints[i + 1] + plq_loss.quad_coef['b'][i + 1]):
+            return False
+
+    return True
+
+
+def check_cutoff(plq_loss):
+    """
+        check whether there exists a cutoff between the knots
+    :return:
+    """
+    # check the cutoff of each piece
+    for i in range(plq_loss.n_pieces - 1):
+        if plq_loss.quad_coef['a'][i] != 0:  # only will happen when the quadratic term is not zero
+            cutpoints = -plq_loss.quad_coef['b'][i] / (2 * plq_loss.quad_coef['a'][i])
+            if plq_loss.cutpoints[i] < cutpoints < plq_loss.cutpoints[i + 1]:  # if the cutoff is between the knots
+                # add the cutoff to the knot list and update the coefficients
+                plq_loss.cutpoints.add(cutpoints, i)
+                plq_loss.quad_coef['a'].add(plq_loss.quad_coef['a'][i], i)
+                plq_loss.quad_coef['b'].add(plq_loss.quad_coef['b'][i], i)
+                plq_loss.quad_coef['c'].add(plq_loss.quad_coef['c'][i], i)
+
+
+def find_min(plq_loss):
+    """
+        find the minimum knots and value of the PLQ function
+    :return:
+    """
+    # find the minimum value and knot
+    out_cut = plq_loss(plq_loss.cutpoints[1:-1])
+    plq_loss.min_val = min(out_cut)
+    plq_loss.min_knot = np.argmin(out_cut) + 1
+    # remove self.min_val from the PLQ function
+    plq_loss.quad_coef['c'] = plq_loss.quad_coef['c'] + (-plq_loss.min_val)
